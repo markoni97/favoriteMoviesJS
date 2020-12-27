24f1917ddb8790de2f829a2f8b079e9f1b88d86f
Marko Radenkovic <code.markoni97@gmail.com>
2020-12-27 16:37:15 +0100

    Added the logic for the confirmAddMovie button

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,10 +2,20 @@
 // const addMovieModal = document.body.children[1];
 const addMovieModal = document.getElementById('add-modal');
 const backdrop = document.getElementById('backdrop');
+const userInputs = addMovieModal.querySelectorAll('input');
 
 //const startAddMovieButton = document.querySelector('header').lastElementChild;
 const startAddMovieButton = document.querySelector('header button');
 const cancleAddMovieButton = addMovieModal.querySelector('.btn--passive');
+const confirmAddMovieButton = cancleAddMovieButton.nextElementSibling;
+
+const moviesList = [];
+
+const clearUserInputs = () => {
+    for(const userInput of userInputs){
+        userInput.value = '';
+    }
+};
 
 const toggleMovieModal = () => {
     addMovieModal.classList.toggle('visible');
@@ -14,6 +24,32 @@ const toggleMovieModal = () => {
 
 const cancleAddMovieHandler = () => {
     toggleMovieModal();
+    clearUserInputs();
+};
+
+const confirmAddMovieHandler = () => {
+    const titleValue = userInputs[0].value;
+    const imgUrlValue = userInputs[1].value;
+    const ratingValue = userInputs[2].value;
+
+    if(
+        titleValue.trim() === '' || 
+        imgUrlValue.trim() === '' || 
+        ratingValue.trim() === '' || 
+        +ratingValue < 1 || 
+        +ratingValue > 5){
+            alert('Please enter valid values (rating between 1 and 5)');
+    } else {
+        const newMovie = {
+            title: titleValue,
+            image: imgUrlValue,
+            rating: ratingValue
+        };
+
+        moviesList.push(newMovie);
+        clearUserInputs();
+        console.log(moviesList);
+    }
 };
 
 const backdrodClickHandler = () => {
@@ -23,3 +59,4 @@ const backdrodClickHandler = () => {
 startAddMovieButton.addEventListener('click', toggleMovieModal);
 cancleAddMovieButton.addEventListener('click', cancleAddMovieHandler);
 backdrop.addEventListener('click', backdrodClickHandler);
+confirmAddMovieButton.addEventListener('click', confirmAddMovieHandler);
